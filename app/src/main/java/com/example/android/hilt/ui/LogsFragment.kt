2840c53d1c8d36e0ea9529f84186306c3b79b017
yoosangyeob <syyoo@joara.com>
2022-08-16 17:23:19 +0900

    Hilt field injection

--- app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
+++ app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
@@ -30,14 +30,17 @@ import com.example.android.hilt.R
 import com.example.android.hilt.data.Log
 import com.example.android.hilt.data.LoggerLocalDataSource
 import com.example.android.hilt.util.DateFormatter
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 /**
  * Fragment that displays the database logs.
  */
+@AndroidEntryPoint
 class LogsFragment : Fragment() {
 
-    private lateinit var logger: LoggerLocalDataSource
-    private lateinit var dateFormatter: DateFormatter
+    @Inject lateinit var logger: LoggerLocalDataSource
+    @Inject lateinit var dateFormatter: DateFormatter
 
     private lateinit var recyclerView: RecyclerView
 
@@ -58,7 +61,7 @@ class LogsFragment : Fragment() {
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
-        populateFields(context)
+//        populateFields(context)
     }
 
     private fun populateFields(context: Context) {
