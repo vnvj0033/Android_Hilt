2814cad240d25e01acaee677fc426e0ebc239800
yoosangyeob <syyoo@joara.com>
2022-08-18 15:38:55 +0900

    code clean up

--- app/src/main/java/com/example/android/hilt/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/hilt/ui/MainActivity.kt
@@ -18,7 +18,6 @@ package com.example.android.hilt.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.android.hilt.LogApplication
 import com.example.android.hilt.R
 import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.Screens
