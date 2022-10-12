f12f8c3dda009997bba1ce0a217f04cd8f6f9020
SangYeob <vnvj0033@gmail.com>
2022-10-12 20:52:12 +0900

    - use sandbox entryPoint

--- app/src/main/java/com/example/android/hilt/sandbox/user/UserFragment.kt
+++ app/src/main/java/com/example/android/hilt/sandbox/user/UserFragment.kt
@@ -7,9 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.android.hilt.sandbox.UserScope
+import dagger.hilt.EntryPoint
+import dagger.hilt.EntryPoints
+import dagger.hilt.InstallIn
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 
-class UserFragment : Fragment() {
+@AndroidEntryPoint
+class UserFragment : Fragment(), UserEvent {
 
     @UserScope
     @Inject lateinit var user: User
@@ -25,5 +31,30 @@ class UserFragment : Fragment() {
 
         Log.d("testsyyoo", "in user")
         Log.d("testsyyoo", user.toString())
+
+        EntryPoints.get(requireContext().applicationContext, UserEvent::class.java).userUpdate()
+
     }
+
+    override fun userUpdate() {
+        Log.d("testsyyoo - userUpdate", "yeah")
+    }
+
+    @InstallIn(SingletonComponent::class)
+    @EntryPoint
+    interface UserEntryPoint {
+        fun userEvent(): UserEvent
+    }
+}
+
+interface UserEvent {
+    fun userUpdate()
+}
+
+class UserRepo(private val userEvent: UserEvent) {
+
+    fun run() {
+        userEvent.userUpdate()
+    }
+
 }
