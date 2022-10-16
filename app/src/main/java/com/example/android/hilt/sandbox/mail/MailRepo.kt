3f8deddf4e92a6988f482faeccf99fa6b5781d7d
yoosangyeob <syyoo@joara.com>
2022-10-16 13:19:41 +0900

    print EntryPoints object hash code

--- app/src/main/java/com/example/android/hilt/sandbox/mail/MailRepo.kt
+++ app/src/main/java/com/example/android/hilt/sandbox/mail/MailRepo.kt
@@ -1,9 +1,13 @@
 package com.example.android.hilt.sandbox.mail
 
+import android.util.Log
 import javax.inject.Inject
 
 
 class MailRepo @Inject constructor(private var action: MailAction) {
+    init {
+        Log.d("MailRepo-hash", hashCode().toString())
+    }
     fun sendMail() {
         action.mailInfo()
     }
