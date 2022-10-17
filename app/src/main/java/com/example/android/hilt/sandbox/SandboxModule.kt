06e4e91a95387de82d732ac623bcd0e26714ef0d
yoosangyeob <syyoo@joara.com>
2022-10-17 19:27:57 +0900

    move MailModule

--- app/src/main/java/com/example/android/hilt/sandbox/SandboxModule.kt
+++ app/src/main/java/com/example/android/hilt/sandbox/SandboxModule.kt
@@ -1,7 +1,5 @@
 package com.example.android.hilt.sandbox
 
-import com.example.android.hilt.sandbox.mail.Mail
-import com.example.android.hilt.sandbox.mail.MailFragment
 import com.example.android.hilt.sandbox.user.User
 import com.example.android.hilt.sandbox.user.UserFragment
 import dagger.Module
@@ -20,45 +18,10 @@ class SandboxUserModule {
     @UserScope
     @Provides
     fun providesUser() = User("user name")
-}
-
-@Module
-@InstallIn(SingletonComponent::class)
-class SandboxMailModule {
-
-    @MailScope
-    @Provides
-    fun providesMailFragment() = MailFragment()
-
-    @MailScope
-    @Provides
-    fun providesMailUser() = User("mail user name")
-
+    
     @Provides
     fun provideSandboxModel() = SandboxModel("base name", 1)
-
-    @MailScope
-    @Provides
-    fun providesMail() = Mail("mail name", "body")
 }
-
-
-@Module
-@InstallIn(SingletonComponent::class)
-class SandboxEntryModule {
-
-    @Provides
-    fun name() = "name"
-
-    @Provides
-    fun mes() = "message"
-
-    @Provides
-    fun mail() = Mail("123", "123")
-}
-
-@Qualifier
-annotation class MailScope
 
 @Qualifier
 annotation class UserScope
--- app/src/main/java/com/example/android/hilt/sandbox/mail/MailComponent.kt
+++ app/src/main/java/com/example/android/hilt/sandbox/mail/MailComponent.kt
@@ -1,10 +1,15 @@
 package com.example.android.hilt.sandbox.mail
 
+import com.example.android.hilt.sandbox.user.User
 import dagger.BindsInstance
+import dagger.Module
+import dagger.Provides
 import dagger.hilt.DefineComponent
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Qualifier
 
 @DefineComponent(parent = SingletonComponent::class)
 interface MailComponent {
@@ -23,3 +28,44 @@ interface UserEntryPoint {
     fun getRepo(): MailRepo
     fun getMail(): Mail
 }
+
+@Module
+@InstallIn(MailComponent::class)
+class SandboxEntryModule {
+
+    @Named("name")
+    @Provides
+    fun provideName() = "name"
+
+    @Named("message")
+    @Provides
+    fun provideMessage() = "message"
+
+    @Provides
+    fun provideMail(
+        @Named("name") name: String,
+        @Named("message") message: String
+    ) = Mail(name, message)
+}
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class SandboxMailModule {
+
+    @MailScope
+    @Provides
+    fun providesMailFragment() = MailFragment()
+
+    @MailScope
+    @Provides
+    fun providesMailUser() = User("mail user name")
+
+
+    @MailScope
+    @Provides
+    fun providesMail() = Mail("mail name", "body")
+}
+
+@Qualifier
+annotation class MailScope
